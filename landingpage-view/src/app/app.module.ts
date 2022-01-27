232756168f8cb602bd26188e013bcce9386c8880
Siddhant Jain <jainsid123456789@gmail.com>
2022-01-27 12:33:25 -0500

    components added

--- landingpage-view/src/app/app.module.ts
+++ landingpage-view/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeatureSliderComponent } from './components/feature-slider/feature-slider.component';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { BannersComponent } from './components/banners/banners.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureSliderComponent,
+    ProductGridComponent,
+    BannersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
